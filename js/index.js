6bd4dcd6c2611f9fbab519e7fca244821f51e8e0
Damian <damian.id89@gmail.com>
2020-02-27 14:14:19 -0600

    agregando JS a admin

--- js/index.js
+++ js/index.js
@@ -1,30 +1,47 @@
 
-// datos de ingreso
+// datos de ingreso INDEX
 var usuario = document.getElementById ('user');
 var contraseña = document.querySelector ('#password');
 
-// boton
+// boton INDEX
 
 var ingreso = document.getElementById ('btn-ingreso');
 
 
-ingreso.addEventListener('click', evaluarUsuario);
 
-function evaluarUsuario(event){
-    event.preventDefault();
-    if(usuario.value === ''){
-        alert('Usuario incorrecto');
-        usuario.focus();
-    }else if(contraseña.value === ''){
-            alert('Contrseña incorrecta');
-            contraseña.focus();
-        }else{
-            
-            
-        }
 
+
+//intento de validar administración
+var identificador = document.getElementById ('id');
+var direccion = document.getElementById ('direccion');
+var colonia = document.getElementById ('colonia');
+var municipio = document.getElementById ('municipio');
+var estado = document.getElementById ('estado');
+var palabraClave = document.getElementById ('palabra-clave');
+
+var buscar = document.getElementById('btn-buscar');
+
+
+
+var enviar = document.getElementById('enviar-admin');
+
+var venta = document.getElementById('opciones');
+
+
+var ventasG = 0;
+var ventasP = 0;
+
+enviar.addEventListener ('click' , enviarVentas);
+
+function enviarVentas(event) {
+    if(venta.value === '2'){
+        console.log('suma para gayol');
+        ventasG++;
+        console.log(ventasG);
+    }
+    if(venta.value === '3'){
+        console.log('suma para providencia');
+        console.log(ventasP);
     }
 
-
-
-
+}
