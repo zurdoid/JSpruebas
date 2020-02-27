2933424d794011f0bb4696e51893d802c1868579
Damian <damian.id89@gmail.com>
2020-02-27 15:44:27 -0600

    first commit

--- js/index.js
+++ js/index.js
@@ -19,29 +19,33 @@ var municipio = document.getElementById ('municipio');
 var estado = document.getElementById ('estado');
 var palabraClave = document.getElementById ('palabra-clave');
 
+//botones
 var buscar = document.getElementById('btn-buscar');
-
-
-
 var enviar = document.getElementById('enviar-admin');
 
+//extras
 var venta = document.getElementById('opciones');
-
-
+var venta2 = document.getElementById('opciones2');
+var venta3 = document.getElementById('opciones3');
 var ventasG = 0;
 var ventasP = 0;
+
+
+//variables de Ventas para imprimir de administración
+var pizarronG = document.querySelector('#vGayol');
+var pizarronP = document.querySelector('#vProvi');
 
 enviar.addEventListener ('click' , enviarVentas);
 
 function enviarVentas(event) {
-    if(venta.value === '2'){
+    if(venta.value === '2' || venta2.value === '2' || venta3.value === '2'){
         console.log('suma para gayol');
         ventasG++;
-        console.log(ventasG);
     }
-    if(venta.value === '3'){
+    if(venta.value === '3' || venta2.value === '3' || venta3.value === '3'){
         console.log('suma para providencia');
-        console.log(ventasP);
+        ventasP++;
     }
-
+        
 }
+
